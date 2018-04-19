8f1ccbf362824d5c797d58313d42499b622efe5d
pradeepkodumuri1 <naveen.w20@gmail.com>
2018-04-19 13:43:01 -0500

    exploring ANgular 5 all options

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from './http-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  constructor(public http:HttpServiceService){
+      var result = http.filmList();
+      console.log(result);
+  }
+  ngOnInit(){
+    this.http.getFilemListfromApi().subscribe((res)=>{
+        console.log(res);
+    });
+    this.http.getFilmListfromApiUsingPromises().then((result)=>{
+      console.log(result);
+    }).catch((err)=>{
+      console.log(err);
+    });
+  }
+}
